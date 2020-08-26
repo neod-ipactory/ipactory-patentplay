2be8123ce093ecfc0e9965b75bbceaf653b96c7f
h-sick <53730691+h-sick@users.noreply.github.com>
2020-08-26 12:24:36 +0900

    Add: 초기세팅

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass">
+*
+  box-sizing: border-box
+  margin: 0
+  padding: 0
+  border: 0
+  list-style: none
+</style>
